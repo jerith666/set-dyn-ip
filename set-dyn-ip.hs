98f45ac9534b05a9cf0b3193565fa4f61a406df9
Matt McHenry <github@matt.mchenryfamily.org>
2016-10-24 23:41:42 -0400

    basic version that appears to connect

---
+++ set-dyn-ip.hs
@@ -0,0 +1,32 @@
+
+import System.Environment
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+
+main = do args <- getArgs
+          case args of
+               []        -> usage
+               (p:[])    -> usage
+               (h:p:[])  -> setDynIpStr h p
+               (x:y:z:_) -> usage
+
+setDynIpStr :: String -> String -> IO ()
+setDynIpStr host portStr = do case (reads portStr) of
+                                   [] -> usage
+                                   [(p, _)] -> setDynIp host p
+
+setDynIp :: String -> Integer -> IO ()
+setDynIp host port = do ios <- socket AF_INET Stream defaultProtocol
+                        hostInfos <- getAddrInfo (Just defaultHints) (Just host) Nothing
+                        setDynIpH hostInfos port ios
+
+setDynIpH :: [AddrInfo] -> Integer -> Socket -> IO ()
+setDynIpH hostInfos port ios =
+  case hostInfos of
+    [] -> usage
+    (hostInfo:_) ->
+      case (addrAddress hostInfo) of
+        SockAddrInet _ addr ->
+          connect ios (SockAddrInet (fromInteger port) addr)
+        _ -> usage
+
+usage = putStrLn "usage: set-dyn-ip host port"
